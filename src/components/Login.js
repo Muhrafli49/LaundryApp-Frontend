6d44bc1480e363077a0a6207dc70e17ad4913084
Rafli <mraflie49@gmail.com>
2024-05-05 10:36:11 +0700

    second commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import '../style/App.css';
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -40,46 +39,35 @@ const Login = () => {
             console.log(error.response.data.message)
             setError(error.response.data.message);
         })
-        // .catch(e => {
-        //     console.log(e.response.data.message)
-        //     setError(e.response.data.message);
-        // })
-        // .catch(valid => {
-        //     console.log(valid)
-        //     SetValid(valid.response.data.message);
-        // })
     }
 
     return (
         <div style={{marginTop: 100}}>
             <div className='container'>
-                <h2 className="text-center mb-4">Bingo Laundry Admin Panel</h2>
-                <p className="text-center mb-4">Selamat Datang</p>
+                <h1 className="text-center mb-4 text-bold">Bingo Laundry</h1>
+                <h5 className="text-center mb-4 text-bold">Selamat Datang !!!</h5>
                 <div className='row justify-content-center'>
                     <div className='col-md-6'>
                         <div className='card p-4'>
-                                {
-                                    error && (
-                                        <div className="alert alert-danger">
-                                            {error}
-                                            {valid}
-                                        </div>
-                                    )
-                                }
+                            {
+                                error && (
+                                    <div className="alert alert-danger">
+                                        {error}
+                                        {valid}
+                                    </div>
+                                )
+                            }
                             <div className='card-body d-flex flex-column justify-content-between'>
                                 <div className='form-group'>
-                                    <label className='text-left label-text'>Username</label>
+                                    <label className='text-left label-text mb-2'>Username</label>
                                     <input placeholder='Username' className='form-control' value={username} onChange={onChangeUsername}/>
                                 </div>
                                 <div className='form-group'>
-                                    <label className='text-left label-text'>Password</label>
+                                    <label className='text-left label-text mb-2'>Password</label>
                                     <input type='password' placeholder='Password' className='form-control' value={password} onChange={onChangePassword}/>
                                 </div>
-
-                                <div className='d-flex justify-content-between'>
-                                    <Link className="btn btn-link mt-4 me-2 text-decoration-none text-dark" to="/register">Belum mendaftar admin?</Link>
-                                    <button className='btn btn-primary mt-3' onClick={submitLogin}>Login</button>
-                                </div>
+    
+                                <button className='btn-login-custom mt-3' onClick={submitLogin}>Login</button>
                             </div>
                         </div>
                     </div>
@@ -87,6 +75,7 @@ const Login = () => {
             </div>
         </div>
     );
+    
 }
 
 export default Login;
